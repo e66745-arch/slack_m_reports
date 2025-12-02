8907fb9aa8ef84d768beb0025a4e320fb816ab0e
e66745-arch <e66745@gmail.com>
2025-12-02 17:02:46 +0900

    UPdate:api

---
+++ api/slack.js
@@ -0,0 +1,21 @@
+import { App } from "@slack/bolt";
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// Slackからのイベントを受信
+app.event("app_mention", async ({ say }) => {
+  await say("Hello from Vercel!");
+});
+
+export default async function handler(req, res) {
+  // Boltのリクエスト処理
+  await app.processEvent({
+    body: req.body,
+    headers: req.headers
+  });
+
+  res.status(200).send("OK");
+}
